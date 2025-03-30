60379c4431f1c0ee60dd9e75721d1db52b3713da
yurimaretti <yurimaretti@hotmail.com>
2025-03-30 15:52:18 -0300

    LCriação do endpoint para listagem de médicos

---
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -0,0 +1,14 @@
+package med.voll.api.medico;
+
+public record DadosListagemMedico(
+        String nome,
+        String crm,
+        String email,
+        Especialidade especialidade
+) {
+
+    public DadosListagemMedico(Medico medico){
+        this(medico.getNome(), medico.getCrm(), medico.getEmail(), medico.getEspecialidade());
+    }
+
+}
