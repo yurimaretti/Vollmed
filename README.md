471eb8c0cf6ee2d7ea267b4cc49fc43a756bc76c
yurimaretti <yurimaretti@hotmail.com>
2025-03-30 17:20:02 -0300

    AdRefatoração do controller para melhor response da API e alteração do application.properties para application.yaml

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Além do que foi posto no curso este projeto também inclui:
 
 1. Variáveis de ambiente.
 2. Tratamento de erros, com response personalizado na API.
-3. Listagem com paginação, e resposta personalizada dessa paginação.
+3. Listagem com paginação e resposta personalizada dessa paginação.
--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,6 +5,7 @@ import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -23,13 +24,17 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
 
+    //Listagem paginada, porém sem os metadados da consulta (numeroPagina, tamanhoPagina, totalResultados e totalPaginas)
+    //Por padrão retorna uma página de tamanho 10 e ordena pela propriedade "nome", mas pode ser alterado nos Query Params
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public List<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
+        return repository.findAll(pageable).stream().map(DadosListagemMedico::new).toList();
     }
 
+    //Listagem paginada, com a mesma funcionalidade de paginação acima, mas também com retorno personalizado de paginação (objetos DTO: resultado e paginacao)
+    //Por padrão retorna uma página de tamanho 10 e ordena pela propriedade "nome", mas pode ser alterado nos Query Params
     @GetMapping("paginando")
-    public DadosPaginacaoGeneric<DadosListagemMedico> listarPaginando(Pageable paginacao){
+    public DadosPaginacaoGeneric<DadosListagemMedico> listarPaginando(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
         Page<DadosListagemMedico> page = repository.findAll(paginacao).map(DadosListagemMedico::new);
 
         return new DadosPaginacaoGeneric<>(page);
