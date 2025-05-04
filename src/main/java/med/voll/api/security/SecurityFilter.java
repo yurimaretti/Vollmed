96536f2299bdec33941ad0443d614f0b51ec9f9f
yurimaretti <yurimaretti@hotmail.com>
2025-05-04 11:13:40 -0300

    Criação da classe de filtro para recuperação do token e futura validação

---
+++ src/main/java/med/voll/api/security/SecurityFilter.java
@@ -0,0 +1,42 @@
+package med.voll.api.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
+        var subject = tokenService.getSubject(tokenJwt);
+
+
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
+        }
+        return authorizationHeader.replace("Bearer ", "");
+    }
+
+}
